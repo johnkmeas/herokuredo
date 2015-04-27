23a0f3daf1a7d6b982331dfd80c10e06aa9e9397
John Meas <johnkmeas@gmail.com>
2015-04-26 19:55:46 -0700

    commit product image update

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424053434) do
+ActiveRecord::Schema.define(version: 20150424055112) do
 
-  create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "colour"
-  end
+# Could not dump table "products" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
 end
