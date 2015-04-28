97dbff411911d98dc5fa10297abf64cf2361c9b5
John Meas <johnkmeas@gmail.com>
2015-04-28 15:14:40 -0700

    model/association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427160945) do
+ActiveRecord::Schema.define(version: 20150428152221) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.float   "total"
+  end
+
+  add_index "orders", ["product_id"], name: "index_orders_on_product_id"
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id"
 
 # Could not dump table "products" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
