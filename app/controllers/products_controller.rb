e05440341aaa864875227e98051e1130055fb3ed
John Meas <johnkmeas@gmail.com>
2015-05-08 08:02:03 -0700

    finally got that colour

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -74,6 +74,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :image_url, :black, :white, :grey, :charcoal, :blue, :red, :green, :yellow, :price)
+      params.require(:product).permit(:name, :description, :image_url, :colour)
     end
 end
