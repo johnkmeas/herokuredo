f1ea661140f4a06904abf0929b64d26a51dead7b
John Meas <johnkmeas@gmail.com>
2015-05-08 23:20:42 -0700

    mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,6 +8,7 @@ class StaticPagesController < ApplicationController
   	@name = params[:name]
   	@email = params[:email]
   	@message = params[:message]
+  	UserMailer.contact_form(@email, @name, @message).deliver
   	ActionMailer::Base.mail(:from => @email,
   		:to => 'johnkmeas@gmail.com',
   		:subject => "A new contact form message from #{@name}",
